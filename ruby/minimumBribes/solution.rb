8196e3b9d4a7dc212a1974014dc1fc830b3cbe49
Dmitry Jum <xfiler@ymail.com>
2023-02-09 00:42:17 -0800

    solves the minimum bribes problem with O(n)

--- ruby/minimumBribes/solution.rb
+++ ruby/minimumBribes/solution.rb
@@ -1,4 +1,14 @@
-require 'pry'
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+#
+# Complete the 'minimumBribes' function below.
+#
+# The function accepts INTEGER_ARRAY q as parameter.
+#
+
 def minimumBribes(q)
   chaos = false
   bribes = 0
@@ -8,17 +18,18 @@ def minimumBribes(q)
       puts "Too chaotic"
       break
     else
-      i.times { |j| bribes += 1 if q[j] > num }
+      ([0,num-2].max..i).each {|j| bribes += 1 if q[j] > num}
     end
   end
   puts bribes unless chaos
 end
 
-a = [1,2,3,5,4,6,7,8]
-b = [4,1,2,3]
-c = [1,2,5,3,4,7,8,6]
-d = [1,2,5,3,7,8,6,4]
-minimumBribes(a)
-minimumBribes(b)
-minimumBribes(c)
-minimumBribes(d)
+t = gets.strip.to_i
+
+t.times do |t_itr|
+    n = gets.strip.to_i
+
+    q = gets.rstrip.split.map(&:to_i)
+
+    minimumBribes q
+end
