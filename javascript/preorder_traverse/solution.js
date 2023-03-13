79c1fee096eae17b4cd89d06812342e64dc55040
Dmitry Jum <xfiler@ymail.com>
2023-03-12 23:47:19 -0700

    adds preorder binary tree traversal solution in javascript

---
+++ javascript/preorder_traverse/solution.js
@@ -0,0 +1,85 @@
+var Tree = function() {
+    this.root = null;
+}
+
+Tree.prototype.insert = function(node, data) {
+    if (node == null){
+    	node = new Node(data);
+    }
+ 	else if (data < node.data){
+        node.left  = this.insert(node.left, data);
+    }
+    else{
+        node.right = this.insert(node.right, data);   
+    }
+
+    return node;
+}
+
+var Node = function(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+}
+
+/* head ends */
+
+/*
+    Node is defined as
+    var Node = function(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+*/
+
+// This is a "method-only" submission.
+// You only need to complete this method.
+
+function preOrder(root) {
+    let dataArray = []
+    function traverse(node) {
+        if (node === null) {
+            return
+        }
+        dataArray.push(node.data)
+        traverse(node.left)
+        traverse(node.right)
+    }
+    traverse(root)
+    console.log(dataArray.join(" "))
+}
+
+/* tail begins */
+
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var _stdin = "";
+var _stdin_array = "";
+var _currentline = 0;
+
+process.stdin.on('data', function(data) {
+    _stdin += data;
+});
+
+process.stdin.on('end', function() {
+    _stdin_array = _stdin.split("\n");
+    solution();
+});
+
+function readLine() {
+    return _stdin_array[_currentline++];
+}
+
+function solution() {
+
+    var tree = new Tree();
+    var n = parseInt(readLine());
+    var m = readLine().split(" ").map(Number);
+    for (var i=0; i<n; i++) {
+        tree.root = tree.insert(tree.root, m[i]);
+    }
+
+    preOrder(tree.root);
+}
