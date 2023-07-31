93e463c147ff22a1658ad98f409c50db6e0be523
Dmitry Jum <xfiler@ymail.com>
2023-07-31 00:22:38 -0700

    solves the last 2 edgecases. It can be the end unless I find more edge cases

--- ruby/isItEncrypted/solution.rb
+++ ruby/isItEncrypted/solution.rb
@@ -3,12 +3,11 @@ def validator(original, encrypted)
   test_map = {}
   return false if original.length != encrypted.length
   original.chars.each_with_index do |char, i|
-    if !test_map[char].nil?
-      if test_map[char] != encrypted[i]
-        return false
-      end
-    else
+    if test_map[char].nil?
+      return false if test_map.values.include? encrypted[i]
       test_map[char] = encrypted[i]
+    else
+      return false if test_map[char] != encrypted[i]
     end
   end
   true
