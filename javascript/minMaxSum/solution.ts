e6b2013b078e7d655ecdf09445402db217de5207
Dmitry Jum <xfiler@ymail.com>
2025-06-17 21:20:01 -0700

    adds 2 miniMaxSum solutions"

--- javascript/minMaxSum/solution.ts
+++ javascript/minMaxSum/solution.ts
@@ -1,12 +1,19 @@
 function miniMaxSum(arr: number[]): void {
   const total = arr.reduce((acc, val) => acc + val, 0)
+  arr.sort()
+  const max = total - arr[0]
+  const min = total - arr[arr.length - 1]
+  console.log([min, max])
+}
+
+function miniMaxSum2(arr: number[]): void { // more performant, because sort is O(n log n) and a single pass is O(n)
+  const total = arr.reduce((acc, val) => acc + val, 0)
   let max = 0
   let min = Number.POSITIVE_INFINITY
   arr.forEach(val => {
     max = Math.max(max, total - val)
     min = Math.min(min, total - val)
   })
-  return [min, max]
-}
-
-console.log(miniMaxSum([1,2,3,4,5]))
+  console.log([min, max])
+}  
+miniMaxSum([1,2,3,4,5])
